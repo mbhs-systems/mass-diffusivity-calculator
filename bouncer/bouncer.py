5eb4e944d89ee7403c0a051730b9705cf74f8f19
= <rytse@protonmail.com>
2018-07-26 18:26:17 -0400

    Move params to dict

--- bouncer/bouncer.py
+++ bouncer/bouncer.py
@@ -2,6 +2,8 @@
 import pybullet as p
 import time
 
+sp = {'t_rad' : 0.5, 't_m' : 1, 't_pos' : [0, 0, 0],'p_rad': 0.15, 'p_m' : 0.01, 'p_pos': [0, 0, 0.9],'lv' : [0, 0, -1],'av' : [0, 0, 0], }
+
 #p.connect(p.GUI)
 p.connect(p.DIRECT)
 
@@ -11,23 +13,18 @@ use_maximal_coordinates = 0
 sim_time = 5
 
 # Create target (i.e. pollen) shape and body
-target_rad = 0.5
-target_mass = 1
-target_pos = [0, 0, 0]
-target = p.createCollisionShape(p.GEOM_SPHERE, radius=target_rad)
-tuid = p.createMultiBody(target_mass,target,visual_shape_id, target_pos, useMaximalCoordinates=use_maximal_coordinates)
+target = p.createCollisionShape(p.GEOM_SPHERE, radius=sp['t_rad'])
+tuid = p.createMultiBody(sp['t_m'],target,visual_shape_id, sp['t_pos'], useMaximalCoordinates=use_maximal_coordinates)
 
 # Create projectile (i.e. air) shape and body
 proj_rad = 0.15
 proj_mass = 0.01
 proj_pos = [0, 0, 0.9]
-proj = p.createCollisionShape(p.GEOM_SPHERE, radius=proj_rad)
-puid = p.createMultiBody(proj_mass,proj,visual_shape_id,proj_pos,useMaximalCoordinates=use_maximal_coordinates)
+proj = p.createCollisionShape(p.GEOM_SPHERE, radius=sp['p_rad'])
+puid = p.createMultiBody(sp['p_m'], proj, visual_shape_id, sp['p_pos'], useMaximalCoordinates=use_maximal_coordinates)
 
 # Give projectile initial velocity
-proj_lin_vel = [0, 0, -1]
-proj_ang_vel = [0, 0, 0]
-p.resetBaseVelocity(puid, proj_lin_vel, proj_ang_vel)
+p.resetBaseVelocity(puid, sp['lv'], sp['av'])
 
 # Start simulation
 p.setGravity(0, 0, 0)	# ignore gravity since this is orientation-agnostic
@@ -39,4 +36,3 @@ while (time.time() - start < sim_time):
     time.sleep(0.001)
 
 print 'end base vel: ' + str(p.getBaseVelocity(tuid))
-
