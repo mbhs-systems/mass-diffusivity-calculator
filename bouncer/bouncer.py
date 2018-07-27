ab1fc3bee2992fec7f98013d637b88df99dd42c5
= <rytse@protonmail.com>
2018-07-27 18:17:03 -0400

    Multithread simulations

--- bouncer/bouncer.py
+++ bouncer/bouncer.py
@@ -1,38 +1,52 @@
 #! /usr/bin/env python
 import pybullet as p
 import time
+import thread
 
-sp = {'t_rad' : 0.5, 't_m' : 1, 't_pos' : [0, 0, 0],'p_rad': 0.15, 'p_m' : 0.01, 'p_pos': [0, 0, 0.9],'lv' : [0, 0, -1],'av' : [0, 0, 0], }
+gst=time.time()
 
-#p.connect(p.GUI)
-p.connect(p.DIRECT)
+SP = {'t_rad' : 0.5, 't_m' : 1, 't_pos' : [0, 0, 0],'p_rad': 0.15, 'p_m' : 0.01, 'p_pos': [0, 0, 0.9],'lv' : [0, 0, -1],'av' : [0, 0, 0], 'sim_time' : 5000, 'pci': 0}
+SP1 = {'t_rad' : 0.5, 't_m' : 1, 't_pos' : [0, 0, 0],'p_rad': 0.15, 'p_m' : 0.01, 'p_pos': [0, 0, 0.9],'lv' : [0, 0, -1],'av' : [0, 0, 0], 'sim_time' : 5000, 'pci': 1}
 
-# Global body params
-visual_shape_id = -1
-use_maximal_coordinates = 0
-sim_time = 5
+p.connect(p.DIRECT, options="physicsClientId=0")
+p.connect(p.DIRECT, options="physicsClientId=1")
 
-# Create target (i.e. pollen) shape and body
-target = p.createCollisionShape(p.GEOM_SPHERE, radius=sp['t_rad'])
-tuid = p.createMultiBody(sp['t_m'],target,visual_shape_id, sp['t_pos'], useMaximalCoordinates=use_maximal_coordinates)
+def simulate(sp):
+    # Global body params
+    visual_shape_id = -1
+    use_maximal_coordinates = 0
+    sim_time = 5
 
-# Create projectile (i.e. air) shape and body
-proj_rad = 0.15
-proj_mass = 0.01
-proj_pos = [0, 0, 0.9]
-proj = p.createCollisionShape(p.GEOM_SPHERE, radius=sp['p_rad'])
-puid = p.createMultiBody(sp['p_m'], proj, visual_shape_id, sp['p_pos'], useMaximalCoordinates=use_maximal_coordinates)
+    # Create target (i.e. pollen) shape and body
+    target = p.createCollisionShape(p.GEOM_SPHERE, radius=sp['t_rad'], physicsClientId=sp['pci'])
+    tuid = p.createMultiBody(sp['t_m'],target,visual_shape_id, sp['t_pos'], useMaximalCoordinates=use_maximal_coordinates, physicsClientId=sp['pci'])
 
-# Give projectile initial velocity
-p.resetBaseVelocity(puid, sp['lv'], sp['av'])
+    # Create projectile (i.e. air) shape and body
+    proj_rad = 0.15
+    proj_mass = 0.01
+    proj_pos = [0, 0, 0.9]
+    proj = p.createCollisionShape(p.GEOM_SPHERE, radius=sp['p_rad'], physicsClientId=sp['pci'])
+    puid = p.createMultiBody(sp['p_m'], proj, visual_shape_id, sp['p_pos'], useMaximalCoordinates=use_maximal_coordinates, physicsClientId=sp['pci'])
 
-# Start simulation
-p.setGravity(0, 0, 0)	# ignore gravity since this is orientation-agnostic
-p.setRealTimeSimulation(1)
+    # Give projectile initial velocity
+    p.resetBaseVelocity(puid, sp['lv'], sp['av'], physicsClientId=sp['pci'])
 
-start = time.time()
-while (time.time() - start < sim_time):
-    keys = p.getKeyboardEvents()
-    time.sleep(0.001)
+    # Start simulation
+    p.setGravity(0, 0, 0, physicsClientId=sp['pci'])    # ignore gravity since this is orientation-agnostic
 
-print 'end base vel: ' + str(p.getBaseVelocity(tuid))
+    start = time.time()
+    for i in range(sp['sim_time']):
+        p.stepSimulation(physicsClientId=sp['pci'])
+        keys = p.getKeyboardEvents()
+        time.sleep(0.001)
+
+    print 'bv' + str(sp['pci']) + ': ' + str(p.getBaseVelocity(tuid, physicsClientId=sp['pci']))
+    print 'time: ' + str(time.time() - gst)
+    return p.getBaseVelocity(tuid, physicsClientId=sp['pci'])
+
+if __name__ == '__main__':
+    thread.start_new_thread(simulate, (SP,))
+    thread.start_new_thread(simulate, (SP1,))
+ 
+    while 1:
+        pass
